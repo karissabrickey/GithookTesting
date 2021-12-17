37767ca1e6b6edcd76d1c6709bef898b4d0da3ba
Karissa Brickey <46540457+karissabrickey@users.noreply.github.com>
2021-12-17 16:07:35 -0600

    making a change that I don't have locally

--- GithookTesting/GithookTesting/Program.cs
+++ GithookTesting/GithookTesting/Program.cs
@@ -6,7 +6,7 @@ namespace GithookTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World, what's up?!");
         }
     }
 }
